cf321db4a0a76364ae2aa56186555327be78b40c
EvgenBershchansky <bershchansky@gmail.com>
2019-11-20 10:09:47 +0000

    Added null resource

--- docker/main.tf
+++ docker/main.tf
@@ -11,6 +11,12 @@ module "container"{
   name = "${lookup(var.container_name, var.env)}"
   external_port = "${lookup(var.external_port, var.env)}"
   internal_port = "${lookup(var.internal_port, var.env)}"
-
 }
+
+resource "null_resource" "null_id"{
+  provisioner "local-exec"{
+    command = "echo ${module.container.container_name}:${module.container.ip} >> container.txt"
+  }
+}
+
 
