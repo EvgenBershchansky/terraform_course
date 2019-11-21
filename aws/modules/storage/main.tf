6d78ea0dd0eae50ff018cb16773654074a514b6d
EvgenBershchansky <bershchansky@gmail.com>
2019-11-21 18:16:19 +0000

    Started terraform with AWS, Created storage module, Changed gitignore

---
+++ aws/modules/storage/main.tf
@@ -0,0 +1,17 @@
+#----storage/main.tf----
+
+# Create a random id
+resource "random_id" "tf_bucket_id"{
+    byte_length = 4
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "tf_code"{
+    bucket = "${var.project_name}-${random_id.tf_bucket_id.hex}"
+    acl = "private"
+    force_destroy = true
+    
+    tags = {
+        Name = "tf_bucket"
+    }
+}
